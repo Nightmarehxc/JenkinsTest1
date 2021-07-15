7e600a4dd143f91bfd6c8b350b89857e581bd6d6
Nightmarehxc <melopeico@gmail.com>
2021-07-15 09:40:09 +0200

    Initial commit

---
+++ src/test/java/com/example/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
